159af8de3d2a4c071adf0a8ee10c93f60977fd6a
Samira Shajahan Borsha <57401425+Samira-Shajahan-Borsha@users.noreply.github.com>
2023-02-17 03:40:51 +0600

    created basic router

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora perspiciatis obcaecati tenetur tempore? Quas ipsam totam iure error, itaque corporis?</p>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Reviews/Reviews.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Reviews = () => {
+    return (
+        <div>
+            <h1>Reviews</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet numquam quam, tenetur rerum neque minima nemo, voluptates recusandae, fugit accusantium veritatis in obcaecati harum libero! Commodi ducimus cumque asperiores saepe?</p>
+        </div>
+    );
+};
+
+export default Reviews;
