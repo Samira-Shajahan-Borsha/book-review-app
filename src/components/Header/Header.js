efafe8253aafad1d08f6a98b2fe76f70f94d3bc7
Samira Shajahan Borsha <57401425+Samira-Shajahan-Borsha@users.noreply.github.com>
2023-02-17 19:43:37 +0600

    styled home component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,11 +3,11 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <nav className='flex sticky top-0 text-lg font-sans text-pink-800 font-medium bg-white items-center justify-center gap-12 py-7 px-12 uppercase shadow-md'>
-            <NavLink to='/'>Home</NavLink>
-            <NavLink to='/reviews'>Reviews</NavLink>
-            <NavLink to='/statistics'>Statistics</NavLink>
-            <NavLink to='/blogs'>Blogs</NavLink>
+        <nav className='flex sticky top-0 text-lg font-sans text-pink-800 font-medium bg-white items-center justify-center gap-12 py-7 px-12 uppercase shadow-md '>
+            <NavLink to='/' className='hover:text-gray-800 hover:underline underline-offset-4 decoration-solid'>Home</NavLink>
+            <NavLink to='/reviews' className='hover:text-gray-800 hover:underline underline-offset-4 decoration-solid'>Reviews</NavLink>
+            <NavLink to='/statistics' className='hover:text-gray-800 hover:underline underline-offset-4 decoration-solid'>Statistics</NavLink>
+            <NavLink to='/blogs' className='hover:text-gray-800 hover:underline underline-offset-4 decoration-solid'>Blogs</NavLink>
         </nav>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,9 +2,26 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora perspiciatis obcaecati tenetur tempore? Quas ipsam totam iure error, itaque corporis?</p>
+        <div className='bg-zinc-100'>
+            <section className='grid grid-cols-1 md:grid-cols-2 mx-auto py-8'>
+                <div className='flex flex-col justify-center w-full md:pl-16 px-4'>
+                    <h1 className='font-extrabold md:text-6xl text-3xl text-gray-800'>It Ends with Us: A Novel</h1>
+                    <p className='text-2xl text-gray-500 mb-2'>August 2, 2016</p>
+                    <h1 className='text-base'>by <span className='text-cyan-700'>Colleen Hoover</span></h1>
+                    <blockquote className='text-lg text-gray-900 text-justify mb-2 mt-4'>
+                        "but sometimes the things that matter to you most are also the things that hurt you the most. And in order to get over that hurt, you have to sever all the extensions that keep you tethered to that pain."</blockquote>
+                    <p className='text-gray-800'>― Colleen Hoover, It Ends with Us</p>
+                </div>
+                <div className='flex w-full justify-center mt-3'>
+                    <img className='w-72' src="https://msmagazine.com/wp-content/uploads/2022/09/91OT29EvAXL-679x1024.jpg" alt="bookImg" />
+                </div>
+            </section>
+            <section>
+                <h1 className='text-2xl text-gray-800 my-8 text-center'>Customer Reviews</h1>
+                <div>
+                    
+                </div>
+            </section>
         </div>
     );
 };
